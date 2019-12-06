f957cedab41adacc74671ec95b89a13ec4f9bf03
VinodSinghNegi <vinod.negi@mail.vinove.com>
2019-12-06 13:22:59 +0530

    first

---
+++ client/src/components/basicDetails.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { MDBFormInline, MDBInput } from "mdbreact";
+import styled from "@emotion/styled";
+import Select from "react-dropdown-select";
+import { connect } from "react-redux";
+import { formData } from "../actions/adduser";
+
+class BasicDetails extends Component {
+  onClick = nr => () => {
+    this.props.formData({ gender: nr });
+  };
+
+  render() {
+    return (
+      <div style={{ padding: "10px" }}>
+        <MDBInput
+          label="User name"
+          icon="user"
+          type="text"
+          value={this.props.name}
+          onChange={e => {
+            this.props.formData({ name: e.target.value });
+          }}
+        />
+        <MDBInput
+          label="User email"
+          icon="envelope"
+          type="email"
+          value={this.props.email}
+          onChange={e => {
+            this.props.formData({ email: e.target.value });
+          }}
+        />
+        {/* <MDBFormInline>
+<MDBInput
+onClick={this.onClick("Male")}
+checked={this.props.gender === "Male" ? true : false}
+label="Male"
+type="radio"
+id="radio1"
+containerClass="mr-5"
+/>
+<MDBInput
+onClick={this.onClick("Female")}
+checked={this.props.gender === "Female" ? true : false}
+label="Female"
+type="radio"
+id="radio2"
+containerClass="mr-5"
+/>
+</MDBFormInline> */}
+        <StyledSelect
+          // placeholder={selectedDesignation.name || "Designation"}
+          color="orange"
+          dropdownHandle="true"
+          dropdownHeight="300px"
+          direction="ltr"
+          values={[{ name: this.props.gender }]}
+          labelField="name"
+          valueField="name"
+          options={[{ name: "Male" }, { name: "FeMale" }]}
+          keepSelectedInList={true}
+          onChange={value => this.props.formData({ gender: value[0].name })}
+          noDataLabel="No matches found"
+        />
+      </div>
+    );
+  }
+}
+
+const StyledSelect = styled(Select)`
+  ${({ dropdownRenderer }) =>
+    dropdownRenderer &&
+    `.react-dropdown-select-dropdown {
+overflow: initial;
+}`}
+`;
+
+const mapStateToProps = state => {
+  const { name, email, gender } = state.addUserForm;
+  return {
+    name,
+    email,
+    gender
+  };
+};
+
+export default connect(mapStateToProps, { formData })(BasicDetails);
---
+++ client/src/components/officialDetails.js
@@ -0,0 +1,132 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Select from "react-dropdown-select";
+import { MDBContainer } from "mdbreact";
+import { connect } from "react-redux";
+import { formData, getDropdown } from "../actions/adduser";
+
+export class OfficialDetails extends React.Component {
+  componentDidMount() {
+    this.props.getDropdown();
+  }
+
+  render() {
+    const {
+      designation,
+      department,
+      kraAttributes,
+      reportingManager,
+      selectedDepartment,
+      selectedDesignation,
+      selectedreportingManager,
+      selectedkraAttributes
+    } = this.props.addUserForm;
+    return (
+      <MDBContainer>
+        <div style={{ maxWidth: "350px", margin: "0 auto" }}>
+          Designation
+          <StyledSelect
+            multi={false}
+            color="orange"
+            searchBy="name"
+            searchable={true}
+            dropdownHandle={true}
+            dropdownHeight="300px"
+            direction="ltr"
+            values={[selectedDesignation]}
+            labelField="name"
+            valueField="name"
+            options={designation}
+            onChange={value =>
+              this.props.formData({ selectedDesignation: value[0] })
+            }
+            noDataLabel="No matches found"
+          />
+        </div>
+        <br />
+        <div style={{ maxWidth: "350px", margin: "0 auto" }}>
+          <StyledSelect
+            placeholder={selectedDepartment.name || "Department"}
+            color="orange"
+            searchBy="name"
+            searchable="true"
+            dropdownHandle="true"
+            dropdownHeight="300px"
+            direction="ltr"
+            values={[selectedDepartment]}
+            labelField="name"
+            valueField="name"
+            options={department}
+            keepSelectedInList={true}
+            onChange={value =>
+              this.props.formData({ selectedDepartment: value[0] })
+            }
+            noDataLabel="No matches found"
+          />
+        </div>
+        <br />
+        <div style={{ maxWidth: "350px", margin: "0 auto" }}>
+          <StyledSelect
+            placeholder={selectedreportingManager.name || "Reporting Manager"}
+            color="orange"
+            searchBy="name"
+            searchable="true"
+            dropdownHandle="true"
+            dropdownHeight="300px"
+            direction="ltr"
+            values={[selectedreportingManager]}
+            labelField="name"
+            valueField="name"
+            options={reportingManager}
+            keepSelectedInList={true}
+            onChange={value =>
+              this.props.formData({ selectedreportingManager: value[0] })
+            }
+            noDataLabel="No matches found"
+          />
+        </div>
+        <br />
+        <div style={{ maxWidth: "350px", margin: "0 auto" }}>
+          <StyledSelect
+            placeholder="KraAttributes"
+            color="orange"
+            searchBy="name"
+            searchable={true}
+            dropdownHandle={true}
+            dropdownHeight="300px"
+            direction="ltr"
+            values={selectedkraAttributes}
+            dropdownPosition="bottom"
+            multi={true}
+            labelField="name"
+            valueField="name"
+            options={kraAttributes}
+            keepSelectedInList={true}
+            onChange={value => {
+              this.props.formData({ selectedkraAttributes: value });
+            }}
+            noDataLabel="No matches found"
+          />
+        </div>
+      </MDBContainer>
+    );
+  }
+}
+
+const StyledSelect = styled(Select)`
+  ${({ dropdownRenderer }) =>
+    dropdownRenderer &&
+    `.react-dropdown-select-dropdown {
+overflow: initial;
+}`}
+`;
+
+const mapStateToProps = state => {
+  return {
+    addUserForm: state.addUserForm
+  };
+};
+
+export default connect(mapStateToProps, { formData, getDropdown })(
+  OfficialDetails
+);
